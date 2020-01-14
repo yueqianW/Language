77239861c42163fda8e00729de634a9b3ca1a45e
yueqianW <ames.yueqian@hotmail.com>
2020-01-14 16:15:43 +0800

    del file

--- .profile
+++
@@ -1,2 +0,0 @@
-
-rt PATH=$PATH:~/.local/bin
--- nester/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='print_lol',
-    version='1.0.0',
-    py_modules=['print_lol'],
-    author='yueqian',
-)
